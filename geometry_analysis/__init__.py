d5541a9c208aee0ca6668451eddb6a75559c78f1
Jiaye Guo <jiaye.guo@choderalab.org>
2019-07-25 11:45:11 -0500

    modifications in the morning

--- geometry_analysis/__init__.py
+++ geometry_analysis/__init__.py
@@ -5,6 +5,7 @@ This is a python package for MolSSI Summer School 2019.
 
 # Add imports here
 from .molecule import *
+from .measure import *
 
 # Handle versioneer
 from ._version import get_versions
